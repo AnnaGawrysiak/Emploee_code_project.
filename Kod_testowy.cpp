06f7fa4811beaf26182c088bb5300cfb3c5522b5
LAPTOP-HKRA8V0S\Ania <anna.m.gawrysiak@gmail.com>
2020-09-22 18:37:35 +0200

    overload ostream << and == operator.

--- Kod_testowy.cpp
+++ Kod_testowy.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Point
 {
 public:
+    friend ostream & operator<<(ostream &OP, const Point &P4);
     int a;
     int b;
 
@@ -13,14 +14,14 @@ public:
         a = a_;
         b = b_;
 
-        cout << "Jestem w konstruktorze z parametrami " << endl;
+      //  cout << "Jestem w konstruktorze z parametrami " << endl;
     }
 
     Point () //konstruktor domyslny. wywola sie zawsze kiedt wywolamy cos bez parametrow. Musi byc zawsze pusty. mozna zapisac = default
     {
     a = 0;
     b = 0;
-    cout << "Jestem w konstruktorze bez parametrow." << endl;
+    //cout << "Jestem w konstruktorze bez parametrow." << endl;
     }
 
     // konstruktor kopiujacy. przyjmuje jako parametr referencje const na ten sam typ. przyjmuje jeden parametr
@@ -30,7 +31,7 @@ public:
            {
             a = P1.a;
             b = P1.b;
-            cout << "Jestem w konstruktorze kopiujacym. " << endl;
+            //cout << "Jestem w konstruktorze kopiujacym. " << endl;
            }
     // operator przypisania =. bedziemy go nadpisywac. Sa zastrzezone operatory.
     // instrukcja, ze bedziemy nadpisywac. tj, jak normalna f(x). opertor musi zwracac. ppowinien zwracac ref. na dany typ.
@@ -47,16 +48,33 @@ public:
 
         // zabezp przed przypisywaniem samego siebie
 
-        cout << "Jestem w operatorze przypisania. " << endl;
+        //cout << "Jestem w operatorze przypisania. " << endl;
         return *this; // musimy wysluskac wartosc . this tj. caly wskaznik
+    }
+
+    bool operator==(const Point & P3) // do bool nie ma referencji
+    {
+        if (a == P3.a && b == P3.b)
+            return true;
+
+        else
+            return false;
     }
 
     ~Point ()
     {
-       cout << " Jestem w destruktorze" << endl;
+       //cout << " Jestem w destruktorze" << endl;
     }
 
 };
+
+ostream & operator<<(ostream &OP, const Point &P4)// referencja jest vbardzo wazna, laczymy operator i znaczki.
+    {
+    OP << "wspolrzedne punktu X: " <<P4.a << ", wspolrzedne punktu Y: " << P4.b << "\n";// zamiast OP jest cout
+
+     return OP;
+    }
+
 
 int main()
 {
@@ -67,11 +85,22 @@ int main()
     Point PunktA(8, 9); // wywolanie listy inicjalizacyjnej. tez jest w konstruktorze
     Point PunktB = PunktA; // wywolanie konstruktora kopiujacego
 
-    cout << PunktB.a << " , " << PunktB.b << endl;
+   // cout << PunktB.a << " , " << PunktB.b << endl;
 
     Point PunktC(1, 1);
 
     PunktA = PunktC;
+
+    Point F = Point(8, 7);
+    Point G (8, 7);
+
+    if (F == G)
+        cout << "Jestesmy identyczni." << endl;
+
+    else
+        cout << "Nie jestesmy rowni." << endl;
+
+    cout << F << endl;
 
     //cout << D.a << " , " << D.b << endl;
 
